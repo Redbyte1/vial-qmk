448c165525a628447425d820082bb7b337fb5770
Spencer McLaughlin <10Spencer@gmail.com>
2023-12-19 13:25:41 -0500

    Create and modify base config files for my own usage

--- keyboards/keychron/q6/ansi_encoder/config.h
+++ keyboards/keychron/q6/ansi_encoder/config.h
@@ -38,3 +38,5 @@
 
 /* Enable NUM_LOCK_LED_INDEX */
 // #define NUM_LOCK_LED_INDEX 37
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
---
+++ keyboards/keychron/q6/ansi_encoder/keymaps/redbyte-vial/rules.mk
@@ -0,0 +1,9 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIALRGB_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+AUTOCORRECT_ENABLE = yes
+#RGB_MATRIX_CUSTOM_USER = yes
+
+VPATH += keyboards/keychron/common
+SRC += keychron_common.c
